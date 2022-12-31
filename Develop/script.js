6c9ec53affffa05842acedde4a67a064c1590370
unknown <charliegarvin90@gmail.com>
2022-12-31 12:34:58 -0700

    added the entire generate password function. Trying to figure out how to capture all of the data.

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,22 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-    prompt("enter a number from 8-128");
-   
-  console.log(prompt);
-  return "password here";
+function generatePassword() { 
+  var number = prompt("enter a password length from 8-128");
+  if (number >= 8 && number <= 128) {
+    console.log(number);
+  } else {
+    return "Not valid input";
+  }
+  
+  do { window.confirm
+   } while (window.confirm === true);
+   window.confirm("Do you want to include lowercase?");
+   window.confirm("Do you want to include uppercase?");
+   window.confirm("Do you want to include numbers?");
+   window.confirm("Do you want to include special characters?");
+
+
 }
 
 // Write password to the #password input
