c511be0760395759aa96cfff37c9a19d78cef15c
unknown <charliegarvin90@gmail.com>
2023-01-01 14:42:08 -0700

    added character arrays for all the different inputs. Reorganized the JS to run correctly. Added new function to collect prompt info.

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,57 @@
+var number = 8;
+var choiceArr = [];
+
+var specialCharArr = ['[',')','(','*','&','%','$','#','@','!'];
+var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','r','x','y','z'];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','R','X','Y','Z'];
+var numberArr = ['1','2','3','4','5','6','7','8','9','0'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() { 
-  var number = prompt("enter a password length from 8-128");
-  if (number >= 8 && number <= 128) {
-    console.log(number);
-  } else {
-    return "Not valid input";
-  }
-  
-  do { window.confirm
-   } while (window.confirm === true);
-   window.confirm("Do you want to include lowercase?");
-   window.confirm("Do you want to include uppercase?");
-   window.confirm("Do you want to include numbers?");
-   window.confirm("Do you want to include special characters?");
-
-
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var rightPrompts = promptCollector();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  if (rightPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() { 
+  var password = "";
+  for (var i = 0; i < number; i++) {
+      var randomIndex = Math.floor(Math.random() * choiceArr.length);
+      password = password + choiceArr[randomIndex];
+    }
+    return password;
+  }
+
+function promptCollector() {
+  choiceArr = [];
+
+  number = parseInt(prompt("Enter a password length from 8-128"));
+
+  if (number < 8 || number > 128) {
+    alert("Not a valid input, please try again.");
+    return false;
+  } 
+  if (window.confirm("Do you want to include lowercase?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+  if (window.confirm("Do you want to include uppercase?")) {
+    choiceArr = choiceArr.concat(upperCaseArr);
+  }
+  if (window.confirm("Do you want to include numbers?")) {
+    choiceArr = choiceArr.concat(numberArr);
+    }
+  if (window.confirm("Do you want to include special characters?")) {
+    choiceArr = choiceArr.concat(specialCharArr);
+  }
+  return true;
+}
